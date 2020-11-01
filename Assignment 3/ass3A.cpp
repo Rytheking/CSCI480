b723013e58154de4f89b89e1f560acd008645086
Rytheking <ryanelliottfleming@gmail.com>
2020-11-01 14:48:50 -0700

    Add quicksort

--- Assignment 3/ass3A.cpp
+++ Assignment 3/ass3A.cpp
@@ -9,9 +9,8 @@
  *A[5]={2,5,6,19,3}
  *index:0,1,2, 3,4}
  * Output: 0,4,1,2,3
+ *
  */
-
-
 //All whitelisted libraries
 #include <iostream>
 #include <fstream>
@@ -24,25 +23,22 @@ using namespace std;
 
 int partition(int arr[], int lower, int upper){
 	int i,j,pivot,temp;
-	pivot=arr[lower];
-	i=lower;
-	j=upper+1;
-	do
+	pivot=arr[upper];
+	i=lower-1;
+	for(int j=lower; j<=upper-1;j++)
 	{
-		cout<<"b";
-		while(arr[i++]>=pivot);
-		while(arr[j--]<=pivot);
-		temp=arr[i];
-		arr[i]=arr[j];
-		arr[j]=temp;
-	}while(i>=j);
-	temp=arr[i];
-	arr[i]=arr[j];
-	arr[j]=temp;
-	temp=arr[lower];
-	arr[lower]=arr[j];
-	arr[j]=temp;
-	return j;
+		if(arr[j] <= pivot)
+		{
+			i++;
+			temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+		}
+	}
+	temp=arr[i+1];
+	arr[i+1]=arr[upper];
+	arr[upper]=temp;
+	return i+1;
 }
 
 void quickSort(int arr[], int lower, int upper){
@@ -59,19 +55,26 @@ int main(int argc, char *argv[])
 {
 	int n;
 	cin>>n;
-	cout<<n;
 	srand(700453594);
-	cout<<"a";
 	int arr[n+1];
-	cout<<"a";
+	ofstream myfile;
+	myfile.open("test.txt");
 	for(int i=0; i<n; i++)
 	{
 		arr[i]=rand()%10000;
+		myfile<<i<<" "<<arr[i]<<"\n";
+
 	}
-	quickSort(arr, 0, n);
+	arr[n+1]=10001;
+	myfile.close();
+	quickSort(arr, 0, n-1);
 	for(int i=0; i<n; i++)
 	{
+		
+		if(i%10==0)
+			cout<<endl;
 		cout<<arr[i]<<"\t";
 	}
+	cout<<endl;
 	return 0;
 }
